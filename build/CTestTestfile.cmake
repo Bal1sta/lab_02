f7545ed227c9acc3ceaf6f99280668d3e51a60c8
Ruslan <bali1sta.0816@gmail.com>
2024-10-05 03:10:34 +0700

    5

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/lAbA/lab_02
+# Build directory: C:/lAbA/lab_02/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(IPFilterTests "C:/lAbA/lab_02/build/test_ip_filter.exe")
+set_tests_properties(IPFilterTests PROPERTIES  _BACKTRACE_TRIPLES "C:/lAbA/lab_02/CMakeLists.txt;26;add_test;C:/lAbA/lab_02/CMakeLists.txt;0;")
